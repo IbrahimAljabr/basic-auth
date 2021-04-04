19f9d6ae1656b4096979c87124e5c98a8b5f920c
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-04-05 02:31:21 +0300

    so long

--- src/auth/router.js
+++ src/auth/router.js
@@ -1,51 +1,31 @@
 'use strict';
 
 const express = require('express');
+const Users = require('./models/users-model.js');
+const auth = require('./middleware/basic.js');
 const router = express.Router();
 
-const app = express();
 
 
-
-router.post('/',signup );
-
-
-function signup(req, res) {
-    console.log('req',req);
-    res.status(201).json('record');
-
-}
-
+const signup = async (req, res) => {
+  try {
+    let record = await Users.addUser(req.body);
+    res.status(201).json(record);
+  } catch (error) {
+    res.status(403).send('Error Creating User');
+  }
+};
 
 
-// app.post('/signup', (req, res) => {
-//     console.log('req',req);
-//     res.status(201).json('record');  
-//   });
+const signin = async (req, res) => {
+  try {
+    res.status(200).json(req.userInfo);
+  } catch (error) {
+    res.status(403).send('Invalid username or password');
+  }
+};
 
-
-// router.post('/signin', (req, res) => {
-
-//     console.log('__HEADERS__', req.headers.authorization);
-//     const encoded = req.headers.authorization.split(' ')[1];
-//     console.log('__ENCODED___', encoded);
-//     const decoded = base64.decode(encoded);
-//     console.log('__DECODED__', decoded);
-//     const [username, password] = decoded.split(':');
-//     console.log('__USERNAME__', username, '__Password__', password);
-//     try {
-//       const user = await Users.findOne({ username });
-//       console.log('__DB_RECORD__', user);
-//       const valid = await bcrypt.compare(password, user.password);
-//       if (valid) {
-//         res.json(user);
-//       } else {
-//         res.status(401).json({ error: 'Invalid User/Password' });
-//       }
-//     } catch (error) {
-//       res.status(401).json({ error: error.message });
-//     }
-// });
-
+router.post('/signup', signup);
+router.post('/signin', auth, signin);
 
 module.exports = router;
